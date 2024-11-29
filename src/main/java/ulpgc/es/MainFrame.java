57579d434d13e1768cb91d9afdb536defb9f5ca8
Saul <exmosaul@gmail.com>
2024-11-29 14:52:46 +0000

    Creación de las clases FileImageLoader, MainFrame y SwingImageDisplay

---
+++ src/main/java/ulpgc/es/MainFrame.java
@@ -0,0 +1,54 @@
+package ulpgc.es;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainFrame extends JFrame {
+    private ImageDisplay imageDisplay;
+    private final Map<String, Command> commands;
+
+
+
+    public MainFrame() {
+        this.commands = new HashMap<>();
+        setTitle("Image-Viewer");
+        setSize(Toolkit.getDefaultToolkit().getScreenSize());
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(800, 600);
+        setLayout(new BorderLayout());
+        add(createImageDisplay());
+        add(createToolbar(), BorderLayout.SOUTH);
+    }
+
+    private Component createToolbar() {
+        JPanel panel = new JPanel();
+        panel.add(createButton("<"));
+        panel.add(createButton(">"));
+        return panel;
+    }
+
+    private Component createButton(String label) {
+        JButton button = new JButton(label);
+        button.addActionListener(e -> commands.get(label).execute());
+        return button;
+    }
+
+    private Component createImageDisplay() {
+        SwingImageDisplay display = new SwingImageDisplay();
+        this.imageDisplay = display;
+        return display;
+    }
+
+
+    public MainFrame add(String name, Command command) {
+        commands.put(name, command);
+        return this;
+    }
+
+    public ImageDisplay imageDisplay() {
+        return imageDisplay;
+    }
+}
